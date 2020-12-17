508f1acd135a52d1dbeeab1983c29462791e8434
joacimwall <joacim.wall@evry.com>
2020-12-17 14:08:03 +0100

    latest

--- src/CoreCom/Examples/Client/TestClient/Program.cs
+++ src/CoreCom/Examples/Client/TestClient/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using WallTec.CoreCom.Client.Models;
 using WallTec.CoreCom.Example.SharedObjects;
@@ -16,16 +15,15 @@ namespace TestClient
             //WPF Server
             //var response = coreComClient.CreateClient("192.168.2.121", 50060).Result;
             //other
-            var response = coreComClient.Connect(new CoreComOptions { ServerAddress="http://localhost:5001", ClientIsMobile=true});
+            var response = coreComClient.Connect(new CoreComOptions { ServerAddress="https://localhost:5001"});
             if (response)
             {
               
 
-                Console.WriteLine("Connection response: " + response.ToString());
-                coreComClient.Register(GetAllProjectsFromDb, CoreComSignatures.ResponseAllProjects, new List<Project>().GetType());
-                Console.WriteLine("Wait 2 sec for connection to backend is upp the press key...");
-                Console.ReadKey();
-                coreComClient.SendAsync(CoreComSignatures.RequestAllProjects);
+                //Console.WriteLine("Connection response: " + response.ToString());
+                //coreComClient.Register(GetAllProjectsFromDb, CoreComSignatures.ResponseAllProjects, new WallTec.CoreCom.Example.SharedObjects.Project().GetType());
+                
+                //coreComClient.SendAsync(CoreComSignatures.RequestAllProjects);
                
                // coreComClient.ShutdownAsync().Wait();
                 Console.WriteLine("Press any key to exit...");
@@ -37,11 +35,7 @@ namespace TestClient
         }
         private static async Task GetAllProjectsFromDb(object value, CoreComUserInfo coreComUserInfo)
         {
-            var projects = value as List<Project>;
-            foreach (var item in projects)
-            {
-                Console.WriteLine(item.Name);
-            }
+            var project = value as Project;
 
         }
     }
