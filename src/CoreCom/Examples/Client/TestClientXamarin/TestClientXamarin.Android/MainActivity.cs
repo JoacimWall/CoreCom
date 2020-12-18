0a38f2b1c25b8b38dc17da6963acee7287a4c885
joacimwall <joacim.wall@evry.com>
2020-12-18 21:35:21 +0100

    fix appname android

--- src/CoreCom/Examples/Client/TestClientXamarin/TestClientXamarin.Android/MainActivity.cs
+++ src/CoreCom/Examples/Client/TestClientXamarin/TestClientXamarin.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace TestClientXamarin.Droid
 {
-    [Activity(Label = "TestClientXamarin", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "CoreCom", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
